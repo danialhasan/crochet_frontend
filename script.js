e13b041746531861ea21b5867498286ddad14c70
Danial Hasan <danialhasan14@gmail.com>
2022-12-17 19:34:15 -0500

    finish mobile view of homepage, and load all projects from localstorage

--- script.js
+++ script.js
@@ -38,13 +38,12 @@ function deleteStep(e, element) {
   });
 }
 function generateUniqueId() {
-  const uniqueId = `uniqueId_${Math.floor(Math.random() * 1e9)}`;
+  const uniqueId = `uniqueId-${Math.floor(Math.random() * 1e9)}`;
   return uniqueId;
 }
-function saveProject() {
+async function saveProject() {
   const projectId = `project_${generateUniqueId()}`;
   const form = document.getElementById("addNewProjectForm");
-  console.log(form);
   const formData = new FormData(form);
   let jsonFormData = { steps: {} };
   for (let [key, value] of formData.entries()) {
@@ -55,6 +54,70 @@ function saveProject() {
       jsonFormData[key] = value;
     }
   }
+  if (jsonFormData.project_image) {
+    jsonFormData.project_image = await convertFileToBase64(
+      jsonFormData.project_image
+    );
+  }
   localStorage.setItem(projectId, JSON.stringify(jsonFormData));
   navigate("/index");
+}
+async function convertFileToBase64(file) {
+  /**
+   * Convert image file to dataURL to store as JSON
+   */
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    let base64;
+    fileReader.onload = () => {
+      base64 = fileReader.result;
+      return resolve(base64);
+    };
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  });
+}
+function loadProjects() {
+  /**
+   * Fetch projects from local storage, and load them in their own
+   * project components. When a project is clicked, open it.
+   */
+
+  localStorage.removeItem("debug");
+  const projectContainer = document.getElementById("projects");
+  const allProjects = { ...localStorage };
+  const projectKeys = Object.keys(allProjects);
+  const projectValues = Object.values(allProjects);
+  for (const key in projectKeys) {
+    /**
+     * For each project, clone the projectTemplate node. Fill it
+     * with data from the project, and then append it to the DOM.
+     */
+    const projectInfo = JSON.parse(projectValues[key]);
+    let stepsContainerClone = document
+      .getElementsByClassName("project")[0]
+      .cloneNode(true);
+
+    if (stepsContainerClone.id === "template-project") {
+      stepsContainerClone.id = projectKeys[key].split("_")[1];
+    }
+    const topDiv = stepsContainerClone.childNodes[3];
+    const bottomDiv = stepsContainerClone.childNodes[5];
+    bottomDiv.childNodes[1].textContent = truncate(projectInfo.title, 18);
+    bottomDiv.childNodes[3].textContent = truncate(projectInfo.name, 24);
+    bottomDiv.childNodes[5].childNodes[1].textContent = truncate(
+      projectInfo.project_description,
+      24
+    );
+    topDiv.childNodes[1].src = projectInfo.project_image;
+
+    topDiv.childNodes[1].srcset ='' 
+    // console.log(projectInfo.project_image);
+
+    projectContainer.appendChild(stepsContainerClone);
+  }
+}
+function truncate(text, limit) {
+  return `${text.slice(0, limit)}...`;
 }
